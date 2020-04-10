e46a2873d8a04f3721181b8320d1ab625fdb5097
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2020-04-10 20:00:56 +0200

    Remove build artifact of API

--- api/src/functions/main.js
+++ api/src/functions/main.js
@@ -1,3 +1,8 @@
-import server from '../server'
+import 'regenerator-runtime/runtime'
+import { ApolloServer } from 'apollo-server-lambda'
+
+import serverConfig from '../server-config'
+
+const server = new ApolloServer(serverConfig)
 
 exports.handler = server.createHandler()
--- api/src/lambda-server.js
+++
@@ -1,6 +0,0 @@
-import 'regenerator-runtime/runtime'
-import { ApolloServer } from 'apollo-server-lambda'
-
-import serverConfig from './server-config'
-
-export default new ApolloServer(serverConfig)
